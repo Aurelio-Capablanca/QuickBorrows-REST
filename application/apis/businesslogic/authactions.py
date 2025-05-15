1bd3f0e0baa5fba2ef45c253af8d3661b6d02a7b
Aurelio <aurelio.iempereur@gmail.com>
2025-05-15 00:56:57 -0600

    moving to JSON
    + standard at Inputs, all inputs are JSON for the sake of the welbeing of the project

--- application/apis/businesslogic/authactions.py
+++ application/apis/businesslogic/authactions.py
@@ -3,11 +3,12 @@ from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
 from application.apis.services.adminservices import get_admins_by_email
+from application.authentication.schemas import LoginInput
 from application.core.security import verify_password, create_access_token
 
 
 
-def authenticate(form_data: OAuth2PasswordRequestForm, db: Session):
+def authenticate(form_data: LoginInput, db: Session):
     user = get_admins_by_email(form_data.username, db)
     if not user or not verify_password(form_data.password, user.adminpass):
         raise HTTPException(status_code=400, detail="Incorrect username or password")
--- application/apis/controllers/authentication_routes.py
+++ application/apis/controllers/authentication_routes.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends
 from fastapi.security import OAuth2PasswordRequestForm
-from application.authentication.schemas import Token
+from application.authentication.schemas import Token, LoginInput
 from application.apis.businesslogic.authactions import authenticate
 from application.database.session import get_db
 from sqlalchemy.orm import Session
@@ -9,5 +9,5 @@ router = APIRouter()
 
 
 @router.post("/login", response_model=Token)
-def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
-    return authenticate(form_data=form_data, db=db)
+def login(payload: LoginInput, db: Session = Depends(get_db)):
+    return authenticate(form_data=payload, db=db)
