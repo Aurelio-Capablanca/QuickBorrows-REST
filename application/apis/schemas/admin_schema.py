dfe1293a8f798d24a2ea38521a77e8c1dc6ddf95
Aurelio <aurelio.iempereur@gmail.com>
2025-05-23 00:12:14 -0600

    Password as Optional
    + Password is optional since it's just a value recalculated at update operation

--- application/apis/schemas/admin_schema.py
+++ application/apis/schemas/admin_schema.py
@@ -2,12 +2,13 @@ from datetime import datetime
 from typing import Optional
 from pydantic import BaseModel
 
+
 class AdministratorSchema(BaseModel):
-    idadministrator:  Optional[int] = None
-    adminname:str
-    adminlastname:str
-    adminemail:str
-    adminpass:str
-    adminphone:str
-    adminstatus:bool
+    idadministrator: Optional[int] = None
+    adminname: str
+    adminlastname: str
+    adminemail: str
+    adminpass: str
+    adminphone: Optional[str] = None
+    adminstatus: bool
     datecreated: Optional[datetime] = None
